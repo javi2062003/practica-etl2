287bb8afea63d1bcb8f47f4436334f330484b916
Javier Aparicio <javi.2062003@gmail.com>
2025-07-22 17:35:15 +0200

    Extracción y Transformación de datos

--- src/main/java/etl/model/ProductoFinal.java
+++ src/main/java/etl/model/ProductoFinal.java
@@ -1,5 +1,81 @@
 package etl.model;
 
+import java.security.Timestamp;
+
 public class ProductoFinal {
+    private Integer id;
+    private String sku;
+    private String nombre;
+    private Double precio;
+    private Integer id_categoria;
+    private String fuenteProveedor;
+    private Timestamp fechaActualizacion;
+    
+    public ProductoFinal(Integer id, String sku, String nombre, Integer precio, Integer id_categoria,
+            String fuenteProveedor, Timestamp fechaActualizacion) {
+        this.id = id;
+        this.sku = sku;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.id_categoria = id_categoria;
+        this.fuenteProveedor = fuenteProveedor;
+        this.fechaActualizacion = fechaActualizacion;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getSku() {
+        return sku;
+    }
+
+    public void setSku(String sku) {
+        this.sku = sku;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+
+    public Integer getId_categoria() {
+        return id_categoria;
+    }
+
+    public void setId_categoria(Integer id_categoria) {
+        this.id_categoria = id_categoria;
+    }
+
+    public String getFuenteProveedor() {
+        return fuenteProveedor;
+    }
+
+    public void setFuenteProveedor(String fuenteProveedor) {
+        this.fuenteProveedor = fuenteProveedor;
+    }
+
+    public Timestamp getFechaActualizacion() {
+        return fechaActualizacion;
+    }
+
+    public void setFechaActualizacion(Timestamp fechaActualizacion) {
+        this.fechaActualizacion = fechaActualizacion;
+    }
     
 }
--- src/main/java/etl/service/ProductTransformer.java
+++ src/main/java/etl/service/ProductTransformer.java
@@ -1,5 +1,102 @@
 package etl.service;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import etl.model.ProductoFinal;
+
+
 public class ProductTransformer {
+    String url = "jdbc:postgresql://localhost:5432/mdm";
+    String user = "postgres";
+    String password = "admin";
+
+ // COMENZAMOS CON LA PRIMERA FUNCIÓN PARA LEER LOS DATOS DE LAS TABLAS DE ORIGEN Y CREANDO
+ // OBJETOS CON ESOS VALORES PARA TRANSFORMARLOS
+
+    public List<ProductoFinal> transformarProductos(){
+         List<Map<String, Object>> productosBrutos = new ArrayList<>();
+
+        try (Connection conn = DriverManager.getConnection(url, user, password)) { 
+
+            //CONSULTA PARA OBTENER LOS DATOS DE LAS TABLAS     
+
+                String sqlA = "SELECT * FROM proveedor_a ";
+                try (Statement stmt = conn.createStatement();
+                 ResultSet rs = stmt.executeQuery(sqlA)) {
+
+            //ACCIÓN PARA LEER LOS VALORES DE CADA LINEA OBTENIDA DE LA CONSULTA Y ALMACENARLOS EN UN ARRAY
+                while (rs.next()) {
+                    Map<String, Object> productoA = new HashMap<>();
+                    productoA.put("id_original", rs.getInt("id_producto"));
+                    productoA.put("nombre_bruto", rs.getString(("nombre_articulo")));
+                    productoA.put("precio_bruto", rs.getDouble("precio_usd"));
+                    productoA.put("categoria_bruta", rs.getString(("categoria")));
+                    productoA.put("fuente", "PROVA");
+
+                    productosBrutos.add(productoA);
+                        }
+                    }
+                String sqlB = "SELECT * FROM proveedor_b ";
+                try (Statement stmt = conn.createStatement();
+                ResultSet rs = stmt.executeQuery(sqlB)) {
+
+                    while (rs.next()) {
+                        Map<String, Object> productoB = new HashMap<>();
+                        productoB.put("id_original", rs.getInt("codigo_producto"));
+                        productoB.put("nombre_bruto", rs.getString(("nombre_producto")));
+                        productoB.put("precio_bruto", rs.getDouble("precio_moneda"));
+                        productoB.put("categoria_bruta", rs.getString(("ref_categoria")));
+                        productoB.put("fuente", "PROVB");
+                    }
+                }
+        } catch (Exception e) {
+            System.err.println("❌ Error al obtener datos de proveedor_a");
+            e.printStackTrace();
+        }
+
     
+        List<ProductoFinal> productosFinales = new ArrayList<>();
+        for (Map<String, Object> bruto : productosBrutos) {
+            String fuente = (String) bruto.get("fuente");
+            String idOriginal = bruto.get("id_original").toString();
+            String sku = fuente + " - " + idOriginal;
+            
+            String nombreLimpio = ((String)
+            bruto.get("nombre_bruto")).trim().toLowerCase();
+
+            double precioEnEur;
+            if(fuente.equals("PROVB") && "USD".equals(bruto.get("moneda_bruta"))) {
+                precioEnEur = (Double) bruto.get("precio_bruto") * 0.92;
+            } else if (fuente.equals("PROVA")) {
+                precioEnEur = (Double) bruto.get("precio_bruto") * 0.92;
+
+            } else {
+                precioEnEur = (Double) bruto.get("precio_bruto");
+
+            }
+
+            int idCategoria = 1;
+
+            ProductoFinal productoLimpio = new ProductoFinal(idCategoria, nombreLimpio, nombreLimpio, idCategoria, idCategoria, nombreLimpio, null);
+
+            productoLimpio.setSku(sku);
+            productoLimpio.setNombre(nombreLimpio);
+            productoLimpio.setPrecio(precioEnEur);
+            productoLimpio.setId_categoria(idCategoria);
+            productoLimpio.setFuenteProveedor(fuente);
+
+            productosFinales.add(productoLimpio);
+
+        }
+        return productosFinales;
+    }        
 }
+
